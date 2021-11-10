fd16f6cadd1252cebc861f9f709b9ce9459f8de1
Sergio <sertorbe@gmail.com>
2021-11-10 12:45:44 +0100

    Add popover

--- src/gui/info_button.rs
+++ src/gui/info_button.rs
@@ -1,10 +1,9 @@
 use relm::Widget;
 use gtk::{prelude::*};
+use relm::{Component, init};
 use relm_derive::{Msg, widget};
 
 use Msg::*;
-
-// TODO: Make info buttons work
 
 #[derive(Msg)]
 pub enum Msg {
@@ -13,32 +12,95 @@ pub enum Msg {
 }
 
 pub struct Model {
-    text: String
+    popover: Component<info_popover::Wdg>
 }
 
 #[widget]
 impl Widget for Wdg {
     fn model() -> Model {
-        Model { text: "".into()}
+        let popover = init::<info_popover::Wdg>(()).expect("Popover");
+        Model {popover}
     }
 
     fn update(&mut self, event: Msg) {
         match event {
-            Pressed => {},
+            Pressed => {
+                self.model.popover.stream().emit(info_popover::Msg::Show);
+            },
             UpdateText(text) => {
                 if let Some(text) = text {
-                    self.model.text = text;
-                    self.widgets.button.set_visible(true);
+                    self.widgets.button.show();
+                    self.model.popover.stream().emit(info_popover::Msg::UpdateText(text));
                 }
-            
+                else {
+                    self.widgets.button.hide();
+                }
             }
+            
+            
         }
     }
 
     view! {
         #[name="button"]
         gtk::Button {
-            label: "Info"
+            image: Some(&gtk::Image::from_icon_name(Some("user-info-symbolic"), gtk::IconSize::Button)),
+            //label: "Info",
+            clicked => Pressed
+        }
+    }
+
+    fn init_view(&mut self) {
+        self.model.popover.stream().emit(info_popover::Msg::SetRelativeTo(self.widgets.button.clone()));
+    }
+}
+
+mod info_popover {
+    use relm::Widget;
+    use gtk::{prelude::*};
+    use relm_derive::{Msg, widget};
+
+    use Msg::*;
+
+    #[derive(Msg)]
+    pub enum Msg {
+        UpdateText(String),
+        SetRelativeTo(gtk::Button),
+        Show
+    }
+
+    pub struct Model {
+        text: String
+    }
+
+    #[widget]
+    impl Widget for Wdg {
+        fn model() -> Model {
+            Model { text: "".into()}
+        }
+
+        fn update(&mut self, event: Msg) {
+            match event {
+                UpdateText(text) => {
+                    self.model.text = text;
+                }
+                SetRelativeTo(relative_to) => {
+                    self.widgets.popover.set_relative_to(Some(&relative_to));
+                }
+                Show => {
+                    self.widgets.popover.show();
+                }                
+            }
+        }
+
+        view! {
+            #[name="popover"]
+            gtk::Popover {
+                gtk::Label {
+                    text: &self.model.text
+                },
+                visible: false
+            }
         }
     }
 }
